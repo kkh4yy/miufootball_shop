5e34fbe804c787cef87174509f15c52e0b838b69
Khayra <khayra.tazkiya@ui.ac.id>
2025-09-10 10:13:11 +0700

    update main

---
+++ main/views.py
@@ -0,0 +1,11 @@
+
+# Create your views here.
+from django.shortcuts import render
+
+def show_main(request):
+    context = {
+        'app_name': 'MiuFootball Shop',
+        'name': '2406428876 - Khayra Tazkiya',
+        'class_name': 'PBP D'
+    }
+    return render(request, 'main/main.html', context)
