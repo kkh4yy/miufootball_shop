842657fe4da72d1a138cb2be807c94f305637ddc
Khayra <khayra.tazkiya@ui.ac.id>
2025-09-17 10:50:29 +0700

    Tugas 3

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from main.views import show_main
-
-app_name = 'main'
+from .views import (
+    show_main,
+    create_product, 
+    product_detail,
+    show_xml, show_json,
+    show_xml_by_id, show_json_by_id
+)
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path("add/", create_product, name="create_product"),
+    path("detail/<int:pk>/", product_detail, name="product_detail"),
+    path("xml/", show_xml, name="show_xml"),
+    path("json/", show_json, name="show_json"),
+    path("xml/<int:id>/", show_xml_by_id, name="show_xml_by_id"),
+    path("json/<int:id>/", show_json_by_id, name="show_json_by_id"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,12 @@
+from django.http import HttpResponse
+from django.core import serializers
+from django.shortcuts import render, redirect, get_object_or_404
+from main.forms import HttpResponse
+from django.core import serializers
 
-# Create your views here.
-from django.shortcuts import render
+from .models import Product
+from .forms import ProductForm
+
 
 def show_main(request):
     context = {
@@ -9,3 +15,38 @@ def show_main(request):
         'class_name': 'PBP D'
     }
     return render(request, 'main.html', context)
+
+def create_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('main:show_main')
+    else:
+        form = ProductForm()
+    return render(request, 'create_product.html', {'form': form})
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product_detail.html', {'product': product})
+
+def show_xml(request):
+    data = serializers.serialize("xml", Product.objects.all())
+    return render(request, 'main.xml', content_type='application/xml')
+
+def show_json(request):
+    data = serializers.serialize("json", Product.objects.all())
+    return HttpResponse(data, content_type='application/json')
+
+def show_xml_by_id(request, id):
+    product = get_object_or_404(Product, pk=id)
+    data = serializers.serialize("xml", [product])
+    return HttpResponse(data, content_type='application/xml')
+
+def show_json_by_id(request, id):
+    product = get_object_or_404(Product, pk=id)
+    data = serializers.serialize("json", [product])
+    return HttpResponse(data, content_type='application/json')
+
+
+
